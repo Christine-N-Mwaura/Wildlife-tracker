a2e01c4167af8e9b74119761b5214d4412e69af6
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-13 16:40:52 +0300

    add id to be generated on save.

--- src/main/java/models/Animals.java
+++ src/main/java/models/Animals.java
@@ -29,9 +29,10 @@ public class Animals {
     public void save(){
         try(Connection con = DB.sql2o.open()) {
             String sql = "INSERT INTO animals (name) VALUES (:name)";
-            con.createQuery(sql)
+             this.id = (int) con.createQuery(sql,true)
                     .addParameter("name",this.name)
-                    .executeUpdate();
+                    .executeUpdate()
+                     .getKey();
 
 
         }
@@ -45,6 +46,10 @@ public class Animals {
 
     public String getName() {
         return name;
+    }
+
+    public int getId() {
+        return id;
     }
 
 }
