9f1324062a696ad226740ad2785d8db0cac75d08
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-13 13:01:32 +0300

    add a constructor for the Animals class.

--- src/main/java/models/Animals.java
+++ src/main/java/models/Animals.java
@@ -2,4 +2,15 @@ package models;
 
 public class Animals {
 
+    private String name;
+    private int id;
+
+    public Animals(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
