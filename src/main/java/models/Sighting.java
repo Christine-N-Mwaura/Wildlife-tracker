5ba8b6a5f82281ff91aafeea3c04488c85c8ac7f
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-14 20:13:59 +0300

    add constructor for class sighting with a get ranger name mehtod.

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -4,4 +4,18 @@ public class Sighting {
 
     private String location;
     private String rangerName;
+    private int animalId;
+
+
+    public Sighting(String rangerName, String location){
+        this.location = location;
+        this.rangerName = rangerName;
+    }
+
+
+
+    public String getRangerName() {
+        return rangerName;
+    }
+
 }
