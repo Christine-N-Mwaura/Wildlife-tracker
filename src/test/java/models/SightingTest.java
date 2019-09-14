55c8b2f31b4903021df1db57f9403865fd34739f
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-14 20:15:00 +0300

    add test for instantiating the sightng class and instantiating ranger name.

--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -1,7 +1,25 @@
 package models;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SightingTest {
+
+    @Test
+    public void sighting_instantiatesCorrectly_true() {
+        Sighting testSighting = setUpNewSighting();
+        assertTrue(testSighting instanceof Sighting);
+    }
+
+    @Test
+    public void getName_returnsRangerNameInstance_true() {
+        Sighting testSighting = setUpNewSighting();
+        assertEquals("Morris",testSighting.getRangerName());
+    }
+
+    public Sighting setUpNewSighting(){
+        return new Sighting("Morris","Zone A");
+    }
 
 }
