d4647c55c7244b57f41ee511eea0574ac6a09096
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-15 07:48:08 +0300

    add delete method to delete fields after each test.

--- src/test/java/models/DatabaseRule.java
+++ src/test/java/models/DatabaseRule.java
@@ -14,8 +14,12 @@ public class DatabaseRule extends ExternalResource {
     @Override
     protected void after() {
         try(Connection con = DB.sql2o.open()){
+            String deleteSightingsQuery = "DELETE FROM sightings *;";
             String deleteAnimalsQuery = "DELETE FROM animals *;";
+
             con.createQuery(deleteAnimalsQuery).executeUpdate();
+            con.createQuery(deleteSightingsQuery).executeUpdate();
+
         }
     }
 }
