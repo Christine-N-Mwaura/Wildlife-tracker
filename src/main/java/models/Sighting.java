6bd8a394c922d11530b68b87bd8b50e525f31e18
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-15 07:47:13 +0300

    add methods to save and retrieve all fields entered.

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -1,24 +1,67 @@
 package models;
 
+import org.sql2o.*;
+import java.util.List;
+import java.util.Objects;
+
 public class Sighting {
 
     private String location;
-    private String rangerName;
+    private String name;
     private int animalId;
+    private int id;
 
 
-    public Sighting(String rangerName, String location){
+
+    public Sighting(String name, String location){
         this.location = location;
-        this.rangerName = rangerName;
+        this.name = name;
+       // this.animalId = animalId;
     }
 
+    @Override
+    public boolean equals(Object otherSighting) {
+        if (this == otherSighting) return true;
+        if (otherSighting == null || getClass() != otherSighting.getClass()) return false;
+        Sighting sighting = (Sighting) otherSighting;
+        return Objects.equals(location, sighting.location) &&
+                Objects.equals(name, sighting.name);
+    }
 
+    public void save(){
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO sightings (name,location) VALUES (:name,:location)";
+            this.id = (int) con.createQuery(sql,true)
+                    .addParameter("name",this.name)
+                    .addParameter("location",this.location)
+                    .executeUpdate()
+                    .getKey();
+
+
+        }
+    }
+    public static List<Sighting> all(){
+        String sql = "SELECT * FROM sightings";
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery(sql).executeAndFetch(Sighting.class);
+
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(location, name);
+    }
 
     public String getRangerName() {
-        return rangerName;
+        return name;
     }
 
     public String getLocation() {
         return location;
+    }
+
+    public int getId() {
+        return id;
     }
 }
