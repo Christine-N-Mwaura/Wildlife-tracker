29bfc22e39145951b0489a5c43017404e3fde024
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-15 07:48:49 +0300

    add test for save and retrieving all fields entered.

--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -1,10 +1,14 @@
 package models;
+import org.junit.Rule;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SightingTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();//checkout database rule uses
+
 
     @Test
     public void sighting_instantiatesCorrectly_true() {
@@ -22,6 +26,25 @@ public class SightingTest {
     public void getLocation_returnsLocationOnClassInitialization_true() {
         Sighting testSighting = setUpNewSighting();
         assertEquals("Zone A",testSighting.getLocation());
+    }
+
+    @Test
+    public void save_insertsObjectsToTheDatabase_Sighting() {
+        Sighting testSighting = setUpNewSighting();
+        testSighting.save();
+        assertTrue(Sighting.all().get(0).equals(testSighting));
+
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfSighting_Sighting() {
+        Sighting firstSighting = setUpNewSighting();
+        firstSighting.save();
+        Sighting secondSighting = new Sighting("Ken","Zone B");
+        secondSighting.save();
+        assertEquals(true,Sighting.all().get(0).equals(firstSighting));
+        assertEquals(true,Sighting.all().get(1).equals(secondSighting));
+
     }
 
     public Sighting setUpNewSighting(){
