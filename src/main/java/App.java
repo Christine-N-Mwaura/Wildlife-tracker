86443d0b4fe399a3e48cafbfe492f9268fb6a318
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-09-17 17:19:20 +0300

    add templates to the project.

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import models.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
